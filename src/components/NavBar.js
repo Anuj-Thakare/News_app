30e9f2eebc173f3813b29d03e045d7bad50fefcd
Anuj Thakare <anujthakare19@gmail.com>
2023-06-16 06:50:07 +0530

    initial commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ export class Navbar extends Component {
     return (
       <div>
         <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-  <a className="navbar-brand" href="#">NewsMonkey</a>
+  <Link className="navbar-brand" to="/">NewsMonkey</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
